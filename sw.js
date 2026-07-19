88463194c46a9efbc72777a8301cc15b338548d1
cseteesta-bit <cseteesta@gmail.com>
2026-07-19 14:59:19 +0600

    Refresh offline cache for expanded app

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const CACHE='bcs100-pro-v1.3.0';
+const CACHE='bcs100-pro-v2.0.0';
 const ASSETS=[
-  './','./index.html','./manifest.json','./css/styles.css',
+  './','./index.html','./manifest.json','./css/styles.css','./css/enhancements.css',
   './assets/icon.svg','./assets/icon-192.svg','./assets/icon-512.svg',
   './js/data.js','./js/syllabus-update.js',
   './js/questions-extra-1.js','./js/questions-extra-2.js','./js/questions-extra-3.js',
-  './js/questions-extra-4.js','./js/questions-extra-5.js','./js/questions-extra-6.js',
-  './js/questions-extra-7.js','./js/app.js'
+  './js/questions-extra-4.js','./js/questions-extra-5.js','./js/questions-extra-6.js','./js/questions-extra-7.js',
+  './js/past-1.js','./js/past-2.js','./js/past-3.js','./js/past-4.js','./js/past-5.js',
+  './js/quiz-core.js','./js/app-base.js','./js/app-views.js','./js/app-quiz.js','./js/app-init.js'
 ];
 
 self.addEventListener('install',event=>{

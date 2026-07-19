7d34a44c6ff89e950ad098cca8ce99dd758eaec6
cseteesta-bit <cseteesta@gmail.com>
2026-07-19 11:54:44 +0600

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='bcs100-pro-v1.0.1';
+const ASSETS=['./','./index.html','./css/styles.css','./js/data.js','./js/app.js','./manifest.json','./assets/icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match('./index.html'))));});

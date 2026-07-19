aa9cb47b2ee15b53aa50c3006d156c4b1685f50e
cseteesta-bit <cseteesta@gmail.com>
2026-07-19 12:48:17 +0600

    Fix official BPSC resource link

--- js/syllabus-update.js
+++ js/syllabus-update.js
@@ -3,7 +3,7 @@
   window.BCS_DATA.meta={appName:'BCS 100 Pro',syllabusVersion:'50th BCS Preliminary Syllabus (2025)',syllabusPublished:'2025-12-09',totalMarks:200,examMinutes:120,note:'Always verify new notices and current affairs from official sources.'};
   window.BCS_DATA.subjects.forEach(subject=>{if(marks[subject.id])subject.marks=marks[subject.id]});
   window.BCS_DATA.resources=[
-    {title:'Official 50th BCS Preliminary Syllabus',url:'https://bpsc.gov.bd/pages/psc-exams/%E0%A7%AB%E0%A7%A6%E0%A6%A4%E0%A6%AE-%E0%A6%AC%E0%A6%BF-%E0%A6%B8%E0%A6%BF-%E0%A6%8F%E0%A6%B8-%E0%A6%AA%E0%A6%B0%E0%A7%80%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A6%BE-%E0%A7%A8%E0%A7%A6%E0%A7%A8%E0%A7%AB-%E0%A6%8F%E0%A6%B0-%E0%A6%AA%E0%A6%BF%E0%A6%B2%E0%A6%BF%E0%A6%AE%E0%A6%BF%E0%A6%A8%E0%A6%BE%E0%A6%B0%E0%A6%BF-%E0%A6%9F%E0%A7%87%E0%A6%B8%E0%A7%8D%E0%A6%9F%E0%A7%87%E0%A6%B0-mcq-type-%E0%A6%B8%E0%A6%BF%E0%A6%B2%E0%A6%BF%E0%A6%AC%E0%A6%BE%E0%A6%B8-299ca4-69568a5d35ce18e1c05ad0af',type:'Official',subject:'all'},
+    {title:'Bangladesh Public Service Commission',url:'https://bpsc.gov.bd/',type:'Official',subject:'all'},
     {title:'BPSC BCS Examination System',url:'https://bpsc.gov.bd/pages/static-pages/691997b3933eb65569dde2bf',type:'Official',subject:'all'},
     {title:'Bangladesh Laws / Constitution',url:'http://bdlaws.minlaw.gov.bd/',type:'Government',subject:'bd'},
     {title:'Bangladesh Bureau of Statistics',url:'https://bbs.portal.gov.bd/',type:'Government',subject:'bd'},

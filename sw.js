50b65d2f8ad6a7ff85a6df43455cafd9038bdf01
cseteesta-bit <cseteesta@gmail.com>
2026-07-19 12:47:49 +0600

    Cache complete 120-question PWA for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE='bcs100-pro-v1.1.0';
-const ASSETS=['./','./index.html','./css/styles.css','./js/data.js','./js/app.js','./manifest.json','./assets/icon.svg'];
+const CACHE='bcs100-pro-v1.2.0';
+const ASSETS=[
+  './','./index.html','./manifest.json','./css/styles.css','./assets/icon.svg',
+  './js/data.js','./js/syllabus-update.js',
+  './js/questions-extra-1.js','./js/questions-extra-2.js','./js/questions-extra-3.js',
+  './js/questions-extra-4.js','./js/questions-extra-5.js','./js/questions-extra-6.js',
+  './js/questions-extra-7.js','./js/app.js'
+];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
